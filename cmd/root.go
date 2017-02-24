d399bdef9e451cf531558018540e3a38e5104862
Silvio Moioli <smoioli@suse.de>
2017-02-24 11:13:12 +0100

    Add proper descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -13,16 +13,8 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "minima",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A Simple Linux Repository Manager",
+	Long: "minima is an application to mirror and manage Linux package repos.",
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
