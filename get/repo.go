6a9b70df50051beb7cb0b9a2995aadd669b122a7
Silvio Moioli <smoioli@suse.de>
2017-02-24 13:29:20 +0100

    Return locations extracted from repomd.xml

--- get/repo.go
+++ get/repo.go
@@ -1,23 +1,40 @@
 package get
 
 import (
-	"io/ioutil"
+	"encoding/xml"
 	"net/http"
 )
 
+type Repomd struct {
+	Data []Data `xml:"data"`
+}
+
+type Data struct {
+	Type     string   `xml:"type,attr"`
+	Location Location `xml:"location"`
+}
+
+type Location struct {
+	Href string `xml:"href,attr"`
+}
+
 // Get downloads one repo and returns its metadata
-func Get(url string) (body string, err error) {
+func Get(url string) (repo map[string]string, err error) {
 	resp, err := http.Get(url + "/repodata/repomd.xml")
 	if err != nil {
 		return
 	}
 
 	defer resp.Body.Close()
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return
+	decoder := xml.NewDecoder(resp.Body)
+	var repomd Repomd
+	err = decoder.Decode(&repomd)
+
+	data := repomd.Data
+	repo = make(map[string]string)
+	for i := 0; i < len(data); i++ {
+		repo[data[i].Type] = data[i].Location.Href
 	}
 
-	body = string(bytes)
 	return
 }
