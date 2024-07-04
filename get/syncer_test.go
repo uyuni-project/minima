3db4498d27e2d147a4619fcef24090723f5f1ae6
Alexander Graul <agraul@suse.com>
2024-07-04 15:12:11 +0200

    Add zst compression type support
    Uses github.com/klauspost/compress/zstd to handle the decompression
    since there is no zstandard support in Go's standard library.
    
    Fixes https://github.com/uyuni-project/minima/issues/36

--- get/syncer_test.go
+++ get/syncer_test.go
@@ -68,6 +68,62 @@ func TestStoreRepo(t *testing.T) {
 	}
 }
 
+func TestStoreRepoZstd(t *testing.T) {
+	directory := filepath.Join(os.TempDir(), "syncer_test")
+	err := os.RemoveAll(directory)
+	if err != nil {
+		t.Error(err)
+	}
+
+	archs := map[string]bool{
+		"x86_64": true,
+	}
+	storage := NewFileStorage(directory)
+	url, err := url.Parse("http://localhost:8080/zstrepo")
+	if err != nil {
+		t.Error(err)
+	}
+	syncer := NewSyncer(*url, archs, storage)
+
+	// first sync
+	err = syncer.StoreRepo()
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedFiles := []string{
+		filepath.Join("repodata", "106c411e443c6b97e7d547f78e32d6d2cdf8e80577999954fdb770d8a21581a0-filelists.xml.zst"),
+		filepath.Join("repodata", "d7fd4cf502d9e1ab8865bc9690c34c3cadcfda0db269f4ddc9c6f026db3f2400-primary.xml.zst"),
+		filepath.Join("repodata", "fa0767ea9359279bb6e8ec93a70cefb3863e4fa4bcbeebbfe755a5ce16c21b94-other.xml.zst"),
+		filepath.Join("repodata", "repomd.xml"),
+		filepath.Join("x86_64", "milkyway-dummy-2.0-1.1.x86_64.rpm"),
+		filepath.Join("x86_64", "orion-dummy-1.1-1.1.x86_64.rpm"),
+		filepath.Join("x86_64", "hoag-dummy-1.1-2.1.x86_64.rpm"),
+		filepath.Join("x86_64", "perseus-dummy-1.1-1.1.x86_64.rpm"),
+		filepath.Join("x86_64", "orion-dummy-sle12-1.1-4.1.x86_64.rpm"),
+	}
+
+	for _, file := range expectedFiles {
+		originalInfo, serr := os.Stat(filepath.Join("testdata", "zstrepo", file))
+		if err != nil {
+			t.Fatal(serr)
+		}
+		syncedInfo, serr := os.Stat(filepath.Join(directory, file))
+		if serr != nil {
+			t.Fatal(serr)
+		}
+		if originalInfo.Size() != syncedInfo.Size() {
+			t.Error("original and synced versions of", file, "differ:", originalInfo.Size(), "vs", syncedInfo.Size())
+		}
+	}
+
+	// second sync
+	err = syncer.StoreRepo()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestStoreDebRepo(t *testing.T) {
 	directory := filepath.Join(os.TempDir(), "syncer_test")
 	err := os.RemoveAll(directory)
